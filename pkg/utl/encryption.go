29dbd2279e1b0ad72097b9f426042e6bcd00352c
ethanmidgley <ethan.c.midgley@gmail.com>
2022-09-22 00:29:03 +0100

    Created basic encryption

---
+++ pkg/utl/encryption.go
@@ -0,0 +1,53 @@
+// Package utl holds any helper functions which may be required
+package utl
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"io"
+)
+
+// Encrypt is a super simple aes gcm cipher encrypt
+func Encrypt(text, key []byte) ([]byte, error) {
+
+	c, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+
+	gcm, err := cipher.NewGCM(c)
+	if err != nil {
+		return nil, err
+	}
+
+	nonce := make([]byte, gcm.NonceSize())
+	if _, err := io.ReadFull(rand.Reader, nonce); err != nil {
+		return nil, err
+	}
+
+	return gcm.Seal(nonce, nonce, text, nil), nil
+
+}
+
+// Decrypt is a super simple aes gcm cipher decrypt
+func Decrypt(text, key []byte) ([]byte, error) {
+
+	c, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+
+	gcm, err := cipher.NewGCM(c)
+	if err != nil {
+		return nil, err
+	}
+
+	nonce := make([]byte, gcm.NonceSize())
+	if _, err := io.ReadFull(rand.Reader, nonce); err != nil {
+		return nil, err
+	}
+
+	return gcm.Seal(nonce, nonce, text, nil), nil
+
+}
